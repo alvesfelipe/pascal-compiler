2488614833e14f33cd7d69f2b9bf2b4a57d4cb06
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-05-21 21:45:59 -0300

    Adds new test file

---
+++ files/file_test_2.pas
@@ -0,0 +1,30 @@
+program Test3; {programa exemplo 2}
+var
+   NUMERO, m : integer;
+   final, teste : integer;
+   bool : boolean;
+   NUMB : real;
+
+procedure findMin(x, y, z: integer; war: integer); 
+var
+    m : integer;
+begin
+   if x < y then
+      m := x
+   else
+      m := y;
+   
+   if z < m then
+      m := z;
+end;
+
+begin  {tente gerar um erro usando um caracter nao permitido.. tipo $}
+   NUMERO := 3 * 5 + 10 - 9;
+
+   if (NUMERO >= 20) and (NUMERO <=90) then begin
+      NUMERO := 10;
+      final := NUMERO + 1;
+   end;
+
+    bool := NUMERO > final;
+end.
