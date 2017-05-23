a84b5f39893bbe9f8fb36cb7a36638d6827fe2a7
Renê <renealves@lavid.ufpb.br>
2017-05-23 01:22:13 -0300

    Added semantic analizer

---
+++ analyzers/semantic.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding=utf-8
+#Lexical Analyzer implementation
+__author__ = "Renê Alves Barbosa"
+__email__ = "renealves@lavid.ufpb.br"
+
+import re
+from lexical import lexicalTable
+
+def loadLexicalTable():
+
+	with open(lexicalTable, 'r') as my_file:
+		file = my_file.read()
+		aux_file = file.splitlines()
+
+	table = []
+	for line in aux_file:
+		table.append(tuple(re.split(r'\t+', line)))
+	
+	return table
+
+table = loadLexicalTable()
+
+def listIdentifierOfScope():
+	idetifierTuple = []
+	numberScope = 0
+	initDeclaration = False
+	countBegin = 0
+	i = 0
+	j = 0
+
+	while i < len(table):
+		if table[i][0] == 'program':
+			numberScope += 1
+		elif table[i][0] == 'procedure':
+			numberScope += 1
+			initDeclaration = True
+		elif table[i][0] == 'begin' and initDeclaration:
+			countBegin += 1
+			initDeclaration = False
+		elif table[i][0] == 'var':
+			initDeclaration = True
+		elif table[i][0] == 'end':
+			if countBegin == 1:
+				numberScope -= 1
+			countBegin -= 1
+		elif table[i][1] == 'identifier':
+			aux = (numberScope, table[i][0], initDeclaration)
+			idetifierTuple.insert(j,aux)
+			j += 1
+		i += 1
+
+	return idetifierTuple
+
+idetifierTuple = listIdentifierOfScope()
+
+def checkIdentifiers(idTuple, val):
+	i = 1
+	countOccurrence = 0
+	while i < len(idetifierTuple):
+		if idetifierTuple[i][1] == idTuple[1] and val != i: #verifica se o nome é igual e que não é ela mesma
+			if idetifierTuple[i][0] == idTuple[0]: #verifico se é do mesmo escopo
+				countOccurrence += 1
+				if idTuple[2]: #verifica se ele é declaração
+					if i < val:
+						print idTuple
+						return False
+		i += 1
+
+	if not idTuple[2] and countOccurrence == 0:
+		print idTuple
+		return False
+
+	return True
+ 
+def scrollList():
+	j = 1
+	while j < len(idetifierTuple):
+		print "teste";
+		if not checkIdentifiers(idetifierTuple[j], j):
+			return False
+		j += 1
+	return True
+
+def main():
+	if not scrollList():
+		print "Error in semantic analyzer"
+		return
+	print "Success, the program has been analyzed"
+
+main();
